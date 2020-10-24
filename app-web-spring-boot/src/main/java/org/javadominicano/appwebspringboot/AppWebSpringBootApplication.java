390f6903d993cbf482be1847bae7e00b81fa6eed
vacax <ccamachog@avathartech.com>
2020-10-24 14:29:22 -0400

    Incluyendo la funcionalida de sesiones.

--- app-web-spring-boot/src/main/java/org/javadominicano/appwebspringboot/AppWebSpringBootApplication.java
+++ app-web-spring-boot/src/main/java/org/javadominicano/appwebspringboot/AppWebSpringBootApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @SpringBootApplication
 public class AppWebSpringBootApplication {
 
@@ -20,6 +22,20 @@ public class AppWebSpringBootApplication {
         @GetMapping(path = "/")
         public String index(){
             return "Hola mundo en Spring Boot";
+        }
+
+        @GetMapping(path = "/contador-sesion")
+        public String contadorSesion(HttpSession session){
+            Integer contador = (Integer) session.getAttribute("contador");
+            if(contador == null){
+                contador = 0;
+            }
+            contador++;
+            session.setAttribute("contador", contador);
+            //
+            String idSesion = session.getId();
+            //
+            return String.format("<h3>Spring Boot - Usted ha visitado %d veces - ID Sesion: %s </h3>", contador, idSesion);
         }
     }
 
